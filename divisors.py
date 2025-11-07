4878ae64e2777d9e3eeef18d4c93d06d2827caaf
Yang Jiwoo <79829948+Jiwoo05@users.noreply.github.com>
2025-11-07 13:27:20 +0900

    Rename variable and improve output formatting
    Refactor variable name from 'n' to 'number' and adjust print formatting.

--- divisors.py
+++ divisors.py
@@ -1,8 +1,9 @@
 import sys
 
-# 명령줄에서 입력받은 첫 번째 숫자
-n = int(sys.argv[1])
+number = int(sys.argv[1]) 
 
-for i in range(1, n + 1):
-    if n % i == 0:
-        print(i)
+for i in range(1, number + 1): 
+    if number % i == 0:         
+        print(i, end=" ")     
+
+print()  
